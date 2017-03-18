4fe5ef6db68ea77862e89df1f91b30cbdf384cbb
EC2 Default User <ec2-user@ip-172-31-13-8.us-west-2.compute.internal>
2017-03-18 04:01:43 +0000

    1st commit

---
+++ primes.c
@@ -0,0 +1,105 @@
+/*
+* Justin J
+* Practicing with prime numbers in C
+* Prompt user to enter total number of prime numbers
+* Calculate prime numbers and print to console for viewing
+*/
+#include<stdio.h>
+#include<stdlib.h>
+
+// function declaration
+void printArray(int*, int);
+int getUserInput();
+
+// entry
+void main() {
+
+  // get user input
+  int primeCount = getUserInput();
+  int* primes = malloc(sizeof(int) * primeCount);
+
+  // keep track of multiples of 6
+  // we are relying on fact that all primes greater than 3 are a multiple of 6 + - 1
+  int multipleOf6 = 6;
+  int i;
+  for(i = 0; i < primeCount;) {
+    // handle cases of i == 0 || i == 1
+    if(i == 0){
+      primes[i++] = 2;
+      continue;
+    }
+    else if(i == 1) {
+      primes[i++] = 3;
+      continue;
+    }
+    // else we know that next prime is multiple of 6 plus + - 1
+    else {
+
+      // check multipleOf6 - 1 to see if it's divisble by any previous primes
+      // note that every non prime is a multiple of some prime
+      int value = multipleOf6 - 1;
+      int j;
+      int isPrime = 1;
+      for(j = 0; j < i - 1; j++) {
+        if(value % primes[j] == 0) {
+          isPrime = 0;
+        }
+      }
+      // if number is prime, add to primes array
+      if(isPrime) {
+        primes[i++] = value;
+      }
+
+      // check multipleOf6 + 1 to see if it's divisble by any previous primes
+      value = multipleOf6 + 1;
+      isPrime = 1;
+      for(j = 0; j < i - 1; j++) {
+        if(value % primes[j] == 0) {
+          isPrime = 0;
+        }
+      }
+      // if number is prime, add to primes array
+       if(isPrime) {
+        primes[i++] = value;
+      }
+
+    }
+   
+    multipleOf6 += 6;
+
+  }
+
+  // display results
+  printArray(primes, primeCount);
+
+}
+
+/*
+* Print array to console with 10 digits per row
+*/
+void printArray(int* a, int length) {
+  printf("\n\nPRINTING ARRAY\n\n");
+  int i;
+  for(i = 0; i < length; i++) {
+    // if at end of first row, add closing bar and new line
+    if(i%10 == 0 && i != 0) {
+      printf("|\n");
+    }
+    printf("|%6d", a[i]);
+  }
+  printf("|\n\n");
+}
+
+/*
+* Prompt user to enter integer
+* Return integer entered by user
+*/
+int getUserInput() {
+  int input;
+  printf("\n\n**********************************\n");
+  printf("* WELCOME TO PRIME NUMBER COUNER *\n");
+ printf("**********************************\n");
+  printf("How many prime numbers do you want to see?\n");
+  scanf("%d", &input);
+  return input;
+}
---
+++ readme.md
@@ -0,0 +1,10 @@
+-----------------------------------------------------------------------------------------------
+##Prime Number Generator in C
+#### Justin J
+-----------------------------------------------------------------------------------------------
+
+- program prompts user to enter total number of prime numbers to print
+- outputs array to console
+- program relies on fact that every prime number is either 1 more or 1 less than multiple of 6
+
+![alt tag](primes.png)
